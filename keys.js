6303bd2248e63d4b95e688a04ea303b11296bec9
Joe Hoffmann <josephjohnhoffmann@gmail.com>
2018-07-18 08:28:19 -0500

    Liri

--- keys.js
+++ keys.js
@@ -1,14 +1,13 @@
-
 console.log('this is loaded');
 
 exports.twitter = {
-    consumer_key: 	5EdVhbmbecTL7HZSXtOp0zA1r,
-    consumer_secret: 	lkOQKnbGC3tFp5SOUfZiTxaQmGCA2K2R0yQnM3q8j9F9oEgqXx,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  };
-  
-  exports.spotify = {
-    id: 38188f86f8e44876ab7ef1618351443c,
-    secret: 571ca5b4bfa947c5b77ed5f1d4966e3b
-  };
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
